8470feeff0bda37ba19a0c95cd548159093b379a
andreaduran1 <134104457+andreaduran1@users.noreply.github.com>
2026-05-19 21:09:24 -0300

    add: rutas para login y portal

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ mongoose.connect(MONGO_URI)
 const productoRoutes = require("./routes/productoRoutes");
 const pedidosRoutes = require("./routes/pedidoRoutes");
 const clienteRoutes = require("./routes/clienteRoutes");
+const portalRoutes = require("./routes/portalRoutes");
+const adminRoutes  = require("./routes/adminRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 // Configura el motor de vistas Pug
 app.set("view engine", "pug");
@@ -29,6 +32,8 @@ app.use(express.static("public"));
 app.use("/productos", productoRoutes);
 app.use("/pedidos", pedidosRoutes);
 app.use("/clientes", clienteRoutes);
+app.use("/portal",   portalRoutes);
+app.use("/admin",    adminRoutes);
 
 // Middleware global de manejo de errores
 app.use((err, req, res, next) => {
